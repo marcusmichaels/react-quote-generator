513139eea84bb4f0fa2655cad5d3508e1beb0476
Marcus Michaels <hello@modestindustries.co>
2020-02-24 18:29:21 +0000

    Updates

---
+++ precache-manifest.0107ef962bb7bde0e2a6783a4411aaf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e6da2151ce9c4c8079719c11ea466d",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "5b150480c5ce16910b6e",
+    "url": "/react-quote-generator/static/css/main.a5d74f9a.chunk.css"
+  },
+  {
+    "revision": "9e61aac79ef1b4811dc3",
+    "url": "/react-quote-generator/static/js/2.6486429e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-quote-generator/static/js/2.6486429e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b150480c5ce16910b6e",
+    "url": "/react-quote-generator/static/js/main.cf71de4f.chunk.js"
+  },
+  {
+    "revision": "6b89d17380ead5f8c2d8",
+    "url": "/react-quote-generator/static/js/runtime-main.6d0ed238.js"
+  }
+]);
